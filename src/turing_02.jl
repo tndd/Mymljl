28028b011b91d12cd509e5e4841149bfad306f79
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2025-01-13 18:07:05 +0900

    .

--- src/turing_02.jl
+++ src/turing_02.jl
@@ -10,10 +10,14 @@ Random.seed!(3)
 # Define Gaussian mixture model.
 w = [0.5, 0.5]
 μ = [-3.5, 0.5]
-mixturemodel = MixtureModel([MvNormal(Fill(μₖ, 2), I) for μₖ in μ], w)
+
+mixturemodel = MixtureModel(
+    [MvNormal(Fill(μₖ, 2), I) for μₖ in μ],
+    w
+)
 
 # We draw the data points.
-N = 60
+N = 600
 x = rand(mixturemodel, N);
 
 scatter(x[1, :], x[2, :]; legend=false, title="Synthetic Dataset")
