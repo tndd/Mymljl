508541d3658abedb26bb22c2726b24bd570e85b0
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2025-01-13 17:55:51 +0900

    tutorial2追加

---
+++ src/turing_02.jl
@@ -0,0 +1,19 @@
+using Distributions
+using FillArrays
+using LinearAlgebra
+using Random
+using StatsPlots
+
+# Set a random seed.
+Random.seed!(3)
+
+# Define Gaussian mixture model.
+w = [0.5, 0.5]
+μ = [-3.5, 0.5]
+mixturemodel = MixtureModel([MvNormal(Fill(μₖ, 2), I) for μₖ in μ], w)
+
+# We draw the data points.
+N = 60
+x = rand(mixturemodel, N);
+
+scatter(x[1, :], x[2, :]; legend=false, title="Synthetic Dataset")
