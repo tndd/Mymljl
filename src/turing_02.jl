97fe05b84778880e498813c49abe332f64933ce1
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2025-01-14 15:37:40 +0900

    .

--- src/turing_02.jl
+++ src/turing_02.jl
@@ -22,14 +22,32 @@ N = 600
 x = rand(mixturemodel, N);
 
 @model function gaussian_mixture_model(x)
+    # Draw the parameters for each of the K=2 clusters from a standard normal distribution.
     K = 2
     μ ~ MvNormal(Zeros(K), I)
-    w ~ Dirichlet(K, 1.0)
-    dist_asigned = Categorical(w)
 
+    # Draw the weights for the K clusters from a Dirichlet distribution with parameters αₖ = 1.
+    w ~ Dirichlet(K, 1.0)
+    # Alternatively, one could use a fixed set of weights.
+    # w = fill(1/K, K)
+
+    # Construct categorical distribution of assignments.
+    distribution_assignments = Categorical(w)
+
+    # (2次元, N)という行列と思いきや逆
     D, N = size(x)
-    dist_clusters = [
-        MvNormal(Fill(μₖ, D), I)
-        for μₖ in μ
-    ]
+    distribution_clusters = [MvNormal(Fill(μₖ, D), I) for μₖ in μ]
+
+    # Draw assignments for each datum and generate it from the multivariate normal distribution.
+    k = Vector{Int}(undef, N)
+    for i in 1:N
+        k[i] ~ distribution_assignments
+        x[:, i] ~ distribution_clusters[k[i]]
+    end
+
+    return k
 end
+
+size(x)
+display(x)
+# model = gaussian_mixture_model(x);
