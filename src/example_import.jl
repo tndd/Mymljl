56cc3c626c63b5344b82250ed5f50c8c7d75e4ac
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2025-01-15 22:49:45 +0900

    コメント追加

--- src/example_import.jl
+++ src/example_import.jl
@@ -1,24 +1,30 @@
 module MainModule
+    # 参照元パッケージは先に存在する必要がある
     module sub_b
         export f_sub_b
         function f_sub_b()
             return 1
         end
     end
+
     module sub_a
         module sub_a1
+            # 絶対パス形式での指定には、頭にMainが必要
             using Main.MainModule.sub_b
             function f_sub_a1()
                 return 10 + f_sub_b()
             end
         end
+
         module sub_a2
+            # モジュールをロードしなくても、内部は評価され実行される
             println("sub_a2 is EMPTY")
         end
     end
 
     function f_main()
-        return MainModule.sub_a.sub_a1.f_sub_a1()
+        # 同階層モジュールはそのまま参照可能
+        return sub_a.sub_a1.f_sub_a1()
     end
 end
 
